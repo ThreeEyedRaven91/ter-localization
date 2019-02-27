a9dc6dd39f02014b9471261f44cbd59417dfcd46
Le Tuan Anh <lekoi9x@gmail.com>
2019-02-27 11:06:22 +0700

    comment WIP route

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ import express from 'express';
 import bodyParser from 'body-parser'
 import Helper from '../common/helpers/index';
 import wordRoute from './routes/word';
-import language from './routes/language';
+// import language from './routes/language';
 import cors from 'cors';
 
 const server = express();
@@ -11,7 +11,7 @@ const port = Helper.config.port;
 server.use(bodyParser.json());
 server.use(cors())
 server.use('/api/word',  wordRoute);
-server.use('/api/language', language);
+// server.use('/api/language', language);
 server.use('/', express.static(__dirname + '/../dist/client'));
 
 const preLanguages = Helper.io.read(Helper.config);
