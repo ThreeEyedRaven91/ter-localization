1076fa3659f2e55ef9c9328fb05be3ce5de4dd09
Le Tuan Anh <lekoi9x@gmail.com>
2019-02-27 11:05:00 +0700

    add auto bootstrap localization folder

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import bodyParser from 'body-parser'
 import Helper from '../common/helpers/index';
 import wordRoute from './routes/word';
+import language from './routes/language';
 import cors from 'cors';
 
 const server = express();
@@ -9,8 +10,9 @@ const port = Helper.config.port;
 
 server.use(bodyParser.json());
 server.use(cors())
-server.use('/api/word', wordRoute);
-server.use('/', express.static(__dirname + '/../client'));
+server.use('/api/word',  wordRoute);
+server.use('/api/language', language);
+server.use('/', express.static(__dirname + '/../dist/client'));
 
 const preLanguages = Helper.io.read(Helper.config);
 Helper.io.write(Helper.config, preLanguages);
